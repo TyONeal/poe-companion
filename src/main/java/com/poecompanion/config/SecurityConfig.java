191db2449be1457de716f64df31a614cb0de6662
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2024-01-06 00:29:46 -0600

    added security config basis to app

--- src/main/java/com/poecompanion/config/SecurityConfig.java
+++ src/main/java/com/poecompanion/config/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.poecompanion.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/profile").authenticated()
+                        .requestMatchers("/","/login").permitAll()
+                        .requestMatchers("/js/**", "/css/**", "/img/**").permitAll())
+                .formLogin(withDefaults());
+        return httpSecurity.build();
+    }
 }
