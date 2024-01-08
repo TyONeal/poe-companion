be7d5dec76bcc18e4864bd71699ca31da7df28ad
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2024-01-07 22:45:00 -0600

    started base weapon setup

--- src/main/java/com/poecompanion/entities/Weapon.java
+++ src/main/java/com/poecompanion/entities/Weapon.java
@@ -1,5 +1,24 @@
 package com.poecompanion.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Weapon extends Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String handedness;
+
 
 }
