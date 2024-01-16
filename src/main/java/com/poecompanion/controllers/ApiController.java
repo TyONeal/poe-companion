8e519341f7c11443a51b0f86850a98b1d46d0f5e
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2024-01-15 18:19:13 -0600

    added some base files for rest controller

---
+++ src/main/java/com/poecompanion/controllers/ApiController.java
@@ -0,0 +1,12 @@
+package com.poecompanion.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("/initial")
+    public String apiCall() {
+        return "rest/initial";
+    }
+}
