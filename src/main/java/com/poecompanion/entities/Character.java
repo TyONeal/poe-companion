699ae4de3895241d2834b444b7db9835eec17280
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2024-01-22 22:19:16 -0600

    working on character

--- src/main/java/com/poecompanion/entities/Character.java
+++ src/main/java/com/poecompanion/entities/Character.java
@@ -12,108 +12,38 @@ public class Character {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false, length = 60, unique = true)
-    private String characterName;
+    @Column
+    private String apiId;
 
-    @Column(nullable = false, length = 3)
-    private long level;
+    @Column
+    private String name;
 
-    @Column(nullable = false, length = 15)
-    private String classType;
+    @Column
+    private String realm;
 
-    @Column(nullable = false, length = 3)
-    private long skillPoints;
+    @Column
+    private String characterClass;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column
+    private String league;
 
-    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "character")
-    private List<StashTab> stashList;
+    @Column
+    private int level;
 
-    public Character() {
-    }
+    @Column
+    private int experience;
 
-    public Character(long id, User user, String characterName, long level, String classType, long skillPoints, List<StashTab> stashList) {
-        this.id = id;
-        this.user = user;
-        this.characterName = characterName;
-        this.level = level;
-        this.classType = classType;
-        this.skillPoints = skillPoints;
-        this.stashList = stashList;
-    }
+    @Column
+    private boolean isRuthless;
 
-    public Character(User user, String characterName, long level, String classType, long skillPoints, List<StashTab> stashList) {
-        this.user = user;
-        this.characterName = characterName;
-        this.level = level;
-        this.classType = classType;
-        this.skillPoints = skillPoints;
-        this.stashList = stashList;
-    }
+    @Column
+    private boolean isExpired;
 
-    public Character(String characterName) {
-        this.characterName = characterName;
-    }
+    @Column
+    private boolean isDeleted;
 
-    public Character(User user) {
-        this.user = user;
-    }
+    @Column
+    private boolean isCurrent;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getCharacterName() {
-        return characterName;
-    }
-
-    public void setCharacterName(String characterName) {
-        this.characterName = characterName;
-    }
-
-    public long getLevel() {
-        return level;
-    }
-
-    public void setLevel(long level) {
-        this.level = level;
-    }
-
-    public String getClassType() {
-        return classType;
-    }
-
-    public void setClassType(String classType) {
-        this.classType = classType;
-    }
-
-    public long getSkillPoints() {
-        return skillPoints;
-    }
-
-    public void setSkillPoints(long skillPoints) {
-        this.skillPoints = skillPoints;
-    }
-
-    public List<StashTab> getStashList() {
-        return stashList;
-    }
-
-    public void setStashList(List<StashTab> stashList) {
-        this.stashList = stashList;
-    }
+    @Column
 }
